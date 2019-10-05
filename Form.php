7e229748b04668f1fd818ca6de19df994758b662
julianarodrigues96 <56053881+julianarodrigues96@users.noreply.github.com>
2019-10-04 21:36:16 -0300

    Add files via upload

---
+++ Form.php
@@ -0,0 +1,20 @@
+<style>
+table, th, td {
+  border: 1px solid black;
+}
+</style>
+
+<table style="border: 1px solid black;">
+
+<tr>
+<th>Nome</th>
+<th>Idade</th>
+<th>Filhos</th>
+</tr>
+<tr>
+<td></td>
+<td></td>
+<td>
+   </td>
+</tr>
+</table>
